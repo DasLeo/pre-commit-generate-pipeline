c8c5cd854335ab38221cf05f4f2c5554d6a4585d
Philipp Leonhardt <philipp.leonhardt@coyoapp.com>
2022-11-28 15:34:23 +0100

    Improved first version

--- generate-pipeline.sh
+++ generate-pipeline.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
-#set -eo pipefail
-
-# DELETE ME FOR DEBUGGING ONLY !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-set -exo pipefail
-
+set -eo pipefail
 
 # globals variables
 # shellcheck disable=SC2155 # No way to assign to readonly variable in separate lines
@@ -12,23 +8,50 @@ readonly SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
 # Store and return last failure so this can validate every directory passed before exiting
 DIFF_ERROR=0
 
+# If on macOS then use gnu-sed from brew
+if [[ $OSTYPE == 'darwin'* ]]; then
+	SED_BINARY="gsed"
+else
+	SED_BINARY="sed"
+fi
+
+# Replace dotted collections in environments.yml as Jinja2 cannot read
+# variables starting with a dot
+$SED_BINARY -i -r 's/\.(otc|gcp):$/\1:/g' environments.yml
+
 for file in "$@"; do
-  generated_pipeline=$(j2 "$file" "$SCRIPT_DIR/../environments.yml")
+  # skip environments.yml as it's only used for data source
+  if [ "$file" == "environments.yml" ]; then continue; fi
+
+  generated_pipeline=$(j2 "$file" "environments.yml")
   if [ $? -ne 0 ]; then
-    echo "ERROR: generating pipeline YAML for '$file'"
+    echo "ERROR: generating pipeline YAML for '$file'. Check log output"
     DIFF_ERROR=1
+    continue
   fi
-  destination_filename=${file%.j2}
+
+  # build destination filename:
+  # foo-bar.yml.j2
+  # will produce
+  # generated-foo-bar.yml
+  DIRNAME="$(dirname "${file}")" ; FILENAME="$(basename "${file}")"
+  destination_filename="$DIRNAME/generated-${FILENAME%.j2}"
+
+  # Check if file exists already
   if [[ -f "$destination_filename" ]]; then
-    if ! echo "$generated_pipeline" | diff "$destination_filename" -; then
+    # Check if there are differences between the currently commited file and
+    # the on j2cli produces
+    if ! echo "$generated_pipeline" | diff "$destination_filename" - >/dev/null 2>&1; then
+      echo "$generated_pipeline" > $destination_filename
+      echo "$destination_filename created, please commit"
       DIFF_ERROR=1
     fi
+  # If file does not exist already, create it
   else
-    echo "$destination_filename has not been generated yet"
     echo "$generated_pipeline" > $destination_filename
+    echo "$destination_filename created, please commit"
     DIFF_ERROR=1
   fi
-  #terraform fmt -diff -check "$file" || DIFF_ERROR=$?
 done
 
 exit ${DIFF_ERROR}
